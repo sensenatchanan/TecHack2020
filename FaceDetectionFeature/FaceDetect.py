a155b6d5a060f80c8453afd5766d47ab6ed3ae69
harini-cookie <63025647+harini-cookie@users.noreply.github.com>
2020-08-15 22:20:32 -0400

    Participants pics added

--- FaceDetectionFeature/FaceDetect.py
+++ FaceDetectionFeature/FaceDetect.py
@@ -1,75 +1,80 @@
+#Stage1: Import all of the installed dependencies
 import cv2
 import numpy as np
-import face_recognition
 import os
+import face_recognition
 from datetime import datetime
+from imutils.video import VideoStream
 
+
+#Stage2: Identify the path of images used for training
 path='ImagesAttendance'
-images = []
-classNames = []
-myList = os.listdir(path)
-print(myList)
+perNames=[]
+imgNames=[]
+studentList=os.listdir(path)
 
-for cl in myList:
-    curImg = cv2.imread(f'{path}/{cl}')
-    images.append(curImg)
-    classNames.append(os.path.splitext (cl)[0])
-print(classNames)
+#Stage3: Encodings for face detection
+def applyEncodings(imgNames):
+    encodingList=[]
+    for pics in imgNames:
+        pics=cv2.cvtColor(pics,cv2.COLOR_BGR2RGB)
+        encodingProcess=face_recognition.face_encodings(pics)[0]
+        encodingList.append(encodingProcess)
 
-def findEncodings(images):
-    encodeList = []
-    for img in images:
-        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img)[0]
-        encodeList.append(encode)
+    return encodingList
+    knownList = applyEncodings(imgNames)
 
-    return encodeList
-
-def markAttendance(name):
+#Stage4: Attendance Marking System
+def takeAttendance(studentNames):
     with open('Attendance.csv','r+') as f:
-        myDataList = f.readlines()
-        nameList=[]
-        for line in myDataList:
-            entry= line.split(',')
-            nameList.append(entry[0])
-        if name not in nameList:
-            now = datetime.now()
-            dtString = now.strftime('%H:%M:%S')
-            f.writelines(f'{name},{dtString}')
-
-        print(myDataList)
+        trackData = f.readlines()
+        snameList=[]
+        for section in trackData:
+            division= section.split(',')
+            snameList.append(division[0])
+        if name not in snameList:
+            current = datetime.current()
+            entryTime = current.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{entryTime}')
 
 
+        print(trackData)
 
-encodeListKnown = findEncodings(images)
-print('Encoding Complete')
+#Stage5: Find student names from the images given
+for pNames in studentList:
+    curPic=cv2.imread(f'{path}/{pNames}')
+    imgNames.append(curPic)
+    perNames.append(os.path.splitext(pNames)[0])
 
-cap = cv2.VideoCapture(0)
+#Stage6: Implementation of demo video
+encodeListKnown = applyEncodings(imgNames)
+cap = VideoStream('ZoomDemoVid.mp4').start()
 
+#Stage7: Convert imgs to rgb and recognize face
 while True:
-    success, img = cap.read()
-    imgS = cv2.resize(img,(0, 0), None, 0.25, 0.25)
-    imgS =cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    pics= cap.read()
+    rgb_pics = pics[:, :, ::-1]
 
-    faceCurFrame = face_recognition.face_locations(imgS)
-    encodeCurFrame = face_recognition.face_encodings(imgS,faceCurFrame)
+    smallPics= cv2.resize(pics,(0, 0), None, 0.25, 0.25)
+    smallPics=cv2.cvtColor(pics, cv2.COLOR_BGR2RGB)
 
-    for encodeFace,faceLoc in zip(encodeCurFrame,faceCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
-        faceDis=face_recognition.face_distance(encodeListKnown,encodeFace)
-        print(faceDis)
-        matchIndex = np.argmin(faceDis)
+    face_locations = face_recognition.face_locations(rgb_pics)
+    faceFrame = face_recognition.face_locations(smallPics)
+    encodingFrame= face_recognition.face_encodings(smallPics,faceFrame)
 
-        if matches[matchIndex]:
-            name = classNames[matchIndex].upper()
+#Stage8: Add box around the faces
+    for faceEncode,faceLoc in zip(encodingFrame,faceFrame):
+        faceMatch= face_recognition.compare_faces(encodeListKnown,faceEncode)
+        faceDis=face_recognition.face_distance(encodeListKnown,faceEncode)
+        matchIndicator= np.argmin(faceDis)
+
+        for top, right, bottom, left in face_locations:
+            cv2.rectangle(pics, (left, top), (right, bottom), (0, 0,255), 2)
+            cv2.imshow('FaceDetect', pics)
+
+#Stage9: Match and attendance feature
+        if faceMatch[matchIndicator]:
+            name = perNames[matchIndicator].upper()
             print(name)
-            y1,x2,y2,x1 = faceLoc
-            y1, x2, y2, x1 = y1*4,x2*4, y2*4, x1*4
-            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
-            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
-            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255, 255),2)
-            markAttendance(name)
-        cv2.imshow('Webcam',img)
-        cv2.waitKey(1)
-
-
+            takeAttendance(name)
+            cv2.waitKey(1)
